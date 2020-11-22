4018447b5cb875885bfd5126a6b8e95157caa8a4
Hagotex <liivamario@gmail.com>
2020-11-22 20:21:41 +0200

    Index

--- vue-solution/src/components/Index.vue
+++ vue-solution/src/components/Index.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>INDEX</h1>
+  <body>
+    <header>
+      <nav>
+        <div class="logo-container">
+          <img src="res/images/logo.png" alt="postIt">
+        </div>
+        <div class="search-container">
+          <input type="text" name="search"><button type="button">Search</button>
+        </div>
+        <div class="dropdown-container">
+          <div class="avatar-container" id="avatar-container">
+            <img src="res/images/avatar.png" id="avatar" class="avatar" alt="Me">	
+          </div>	
+          <div class="dropdown" id="dropdown">	
+            <p id="user_info">Some fake data</p>	
+            <a href="browse.html">Browse</a>	
+            <a href="login.html">Log out</a>	
+          </div>
+        </div>
+      </nav>
+    </header>
+  </body>
 </template>
